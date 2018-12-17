99ab1c6d8b4069a28ef7665cf5d6f22b7cd03f04
Nabarun Sen <nabarun@Nabaruns-iMac.local>
2018-12-17 22:54:00 +0800

    first commit

---
+++ src/main/java/com/example/nabarun/NabarunApplication.java
@@ -0,0 +1,14 @@
+package com.example.nabarun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NabarunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NabarunApplication.class, args);
+	}
+
+}
+
